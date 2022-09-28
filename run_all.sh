a2c943e22b528914dd6e89297214965921b136ff
bayonato89 <72937672+bayonato89@users.noreply.github.com>
2022-09-28 13:51:45 +0100

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -5,4 +5,4 @@ unzip ./forecasts/access_forecasts.zip
 echo ''
 echo 'Running the computations'
 echo '============================'
-python ./code/reproducibility_global_vs_regional3.py
+python ./code/reproducibility_global_vs_regional.py
